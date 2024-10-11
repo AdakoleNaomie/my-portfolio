50e9eec11de9cefb271d7e242fa1373e59799a14
Adakole Naomie <adakolenaomi049@gmail.com>
2024-10-11 16:04:27 -0700

    third commit

--- scri.js
+++ scri.js
@@ -1,4 +1,4 @@
-const cont1 = document.querySelectorAll(".cont1")
+const txt1 = document.querySelectorAll(".txt1")
 
 const observer = new IntersectionObserver(entries => {
     
@@ -13,11 +13,49 @@ const observer = new IntersectionObserver(entries => {
     // threshold: 1,
     rootMargin:"-100px",
 })
-cont1.forEach(cont1 =>{
-    observer.observe(cont1)
+txt1.forEach(txt1 =>{
+    observer.observe(txt1)
 })
 
-const cont2 = document.querySelectorAll(".cont2")
+const txt2 = document.querySelectorAll(".txt2")
+
+const observer2 = new IntersectionObserver(entries => {
+    
+    entries.forEach(entry =>{
+        entry.target.classList.toggle("show", entry.isIntersecting)
+
+        // the berlow commented code is for if you don't want the effect to be repetitive
+        // if (entry.isIntersecting) observer.unobserve(entry.target)
+        console.log(entries)
+    })
+}, {
+    // threshold: 1,
+    rootMargin:"-100px",
+})
+txt2.forEach(txt2 =>{
+    observer.observe(txt2)
+})
+
+const txt3 = document.querySelectorAll(".txt3")
+
+const observer3 = new IntersectionObserver(entries => {
+    
+    entries.forEach(entry =>{
+        entry.target.classList.toggle("show", entry.isIntersecting)
+
+        // the berlow commented code is for if you don't want the effect to be repetitive
+        // if (entry.isIntersecting) observer.unobserve(entry.target)
+        console.log(entries)
+    })
+}, {
+    // threshold: 1,
+    rootMargin:"-100px",
+})
+txt3.forEach(txt3 =>{
+    observer.observe(txt3)
+})
+
+const txts = document.querySelectorAll(".txts")
 
 const observer4 = new IntersectionObserver(entries => {
     
@@ -32,11 +70,11 @@ const observer4 = new IntersectionObserver(entries => {
     // threshold: 1,
     rootMargin:"-100px",
 })
-cont2.forEach(cont2 =>{
-    observer.observe(cont2)
+txts.forEach(txts =>{
+    observer.observe(txts)
 })
 
-const cont3 = document.querySelectorAll(".cont3")
+const txts2 = document.querySelectorAll(".txts2")
 
 const observer5 = new IntersectionObserver(entries => {
     
@@ -51,49 +89,11 @@ const observer5 = new IntersectionObserver(entries => {
     // threshold: 1,
     rootMargin:"-100px",
 })
-cont3.forEach(cont3 =>{
-    observer.observe(cont3)
+txts2.forEach(txts2 =>{
+    observer.observe(txts2)
 })
 
-const cont4 = document.querySelectorAll(".cont4")
-
-const observer6 = new IntersectionObserver(entries => {
-    
-    entries.forEach(entry =>{
-        entry.target.classList.toggle("show", entry.isIntersecting)
-
-        // the berlow commented code is for if you don't want the effect to be repetitive
-        // if (entry.isIntersecting) observer.unobserve(entry.target)
-        console.log(entries)
-    })
-}, {
-    // threshold: 1,
-    rootMargin:"-100px",
-})
-cont4.forEach(cont4 =>{
-    observer.observe(cont4)
-})
-
-const cont5 = document.querySelectorAll(".cont5")
-
-const observer7 = new IntersectionObserver(entries => {
-    
-    entries.forEach(entry =>{
-        entry.target.classList.toggle("show", entry.isIntersecting)
-
-        // the berlow commented code is for if you don't want the effect to be repetitive
-        // if (entry.isIntersecting) observer.unobserve(entry.target)
-        console.log(entries)
-    })
-}, {
-    // threshold: 1,
-    rootMargin:"-100px",
-})
-cont5.forEach(cont5 =>{
-    observer.observe(cont5)
-})
-
-const cont6 = document.querySelectorAll(".cont6")
+const txts3 = document.querySelectorAll(".txts3")
 
 const observer8 = new IntersectionObserver(entries => {
     
@@ -108,13 +108,13 @@ const observer8 = new IntersectionObserver(entries => {
     // threshold: 1,
     rootMargin:"-100px",
 })
-cont6.forEach(cont6 =>{
-    observer.observe(cont6)
+txts3.forEach(txts3 =>{
+    observer.observe(txts3)
 })
 
-const sk = document.querySelectorAll(".sk")
+const txts4 = document.querySelectorAll(".txts4")
 
-const observer9 = new IntersectionObserver(entries => {
+const observer6 = new IntersectionObserver(entries => {
     
     entries.forEach(entry =>{
         entry.target.classList.toggle("show", entry.isIntersecting)
@@ -127,6 +127,127 @@ const observer9 = new IntersectionObserver(entries => {
     // threshold: 1,
     rootMargin:"-100px",
 })
-sk.forEach(sk =>{
-    observer.observe(sk)
+txts4.forEach(txts4 =>{
+    observer.observe(txts4)
+})
+
+const txts5 = document.querySelectorAll(".txts5")
+
+const observer7 = new IntersectionObserver(entries => {
+    
+    entries.forEach(entry =>{
+        entry.target.classList.toggle("show", entry.isIntersecting)
+
+        // the berlow commented code is for if you don't want the effect to be repetitive
+        // if (entry.isIntersecting) observer.unobserve(entry.target)
+        console.log(entries)
+    })
+}, {
+    // threshold: 1,
+    rootMargin:"-100px",
+})
+txts5.forEach(txts5 =>{
+    observer.observe(txts5)
+})
+
+const image1 = document.querySelectorAll(".image1")
+
+const observer9 = new IntersectionObserver(entries => {
+    
+    entries.forEach(entry =>{
+        entry.target.classList.toggle("show", entry.isIntersecting)
+
+       
+        console.log(entries)
+    })
+}, {
+    rootMargin:"-100px",
+})
+image1.forEach(image1 =>{
+    observer.observe(image1)
+})
+
+const image2 = document.querySelectorAll(".image2")
+
+const observer10 = new IntersectionObserver(entries => {
+    
+    entries.forEach(entry =>{
+        entry.target.classList.toggle("show", entry.isIntersecting)
+
+       
+        console.log(entries)
+    })
+}, {
+    rootMargin:"-100px",
+})
+image2.forEach(image2 =>{
+    observer.observe(image2)
+})
+
+const image3 = document.querySelectorAll(".image3")
+
+const observer11 = new IntersectionObserver(entries => {
+    
+    entries.forEach(entry =>{
+        entry.target.classList.toggle("show", entry.isIntersecting)
+
+       
+        console.log(entries)
+    })
+}, {
+    rootMargin:"-100px",
+})
+image3.forEach(image3 =>{
+    observer.observe(image3)
+})
+
+const image6 = document.querySelectorAll(".image6")
+
+const observer12 = new IntersectionObserver(entries => {
+    
+    entries.forEach(entry =>{
+        entry.target.classList.toggle("show", entry.isIntersecting)
+
+       
+        console.log(entries)
+    })
+}, {
+    rootMargin:"-100px",
+})
+image6.forEach(image6 =>{
+    observer.observe(image6)
+})
+
+const image7 = document.querySelectorAll(".image7")
+
+const observer13 = new IntersectionObserver(entries => {
+    
+    entries.forEach(entry =>{
+        entry.target.classList.toggle("show", entry.isIntersecting)
+
+       
+        console.log(entries)
+    })
+}, {
+    rootMargin:"-100px",
+})
+image7.forEach(image7 =>{
+    observer.observe(image7)
+})
+
+const text1 = document.querySelectorAll(".text1")
+
+const observer14 = new IntersectionObserver(entries => {
+    
+    entries.forEach(entry =>{
+        entry.target.classList.toggle("show", entry.isIntersecting)
+
+       
+        console.log(entries)
+    })
+}, {
+    rootMargin:"-100px",
+})
+text1.forEach(text1=>{
+    observer.observe(text1)
 })
