636494b5ad5c371802caf9bedf926224f8ecbd88
Adakole Naomie <adakolenaomi049@gmail.com>
2024-11-26 06:14:44 -0800

    commit

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const txt1 = document.querySelectorAll(".txt1")
+const container = document.querySelectorAll(".container")
 
 const observer = new IntersectionObserver(entries => {
     
@@ -13,12 +13,12 @@ const observer = new IntersectionObserver(entries => {
     // threshold: 1,
     rootMargin:"-100px",
 })
-txt1.forEach(txt1 =>{
-    observer.observe(txt1)
+container.forEach(container =>{
+    observer.observe(container)
 })
 
 
-const textbox = document.querySelectorAll(".textbox")
+const firsttext = document.querySelectorAll(".firsttext")
 
 const observer1 = new IntersectionObserver(entries => {
     
@@ -33,214 +33,7 @@ const observer1 = new IntersectionObserver(entries => {
     // threshold: 1,
     rootMargin:"-100px",
 })
-textbox.forEach(textbox =>{
-    observer.observe(textbox)
-})
-
-const b1 = document.querySelectorAll(".b1")
-
-const observer2 = new IntersectionObserver(entries => {
-    
-    entries.forEach(entry =>{
-        entry.target.classList.toggle("show", entry.isIntersecting)
-
-        // the berlow commented code is for if you don't want the effect to be repetitive
-        // if (entry.isIntersecting) observer.unobserve(entry.target)
-        console.log(entries)
-    })
-}, {
-    // threshold: 1,
-    rootMargin:"-100px",
-})
-b1.forEach(b1 =>{
-    observer.observe(b1)
-})
-
-const b2 = document.querySelectorAll(".b2")
-
-const observer7 = new IntersectionObserver(entries => {
-    
-    entries.forEach(entry =>{
-        entry.target.classList.toggle("show", entry.isIntersecting)
-
-        // the berlow commented code is for if you don't want the effect to be repetitive
-        // if (entry.isIntersecting) observer.unobserve(entry.target)
-        console.log(entries)
-    })
-}, {
-    // threshold: 1,
-    rootMargin:"-100px",
-})
-b2.forEach(b2 =>{
-    observer.observe(b2)
-})
-
-const b3 = document.querySelectorAll(".b3")
-
-const observer8 = new IntersectionObserver(entries => {
-    
-    entries.forEach(entry =>{
-        entry.target.classList.toggle("show", entry.isIntersecting)
-
-        // the berlow commented code is for if you don't want the effect to be repetitive
-        // if (entry.isIntersecting) observer.unobserve(entry.target)
-        console.log(entries)
-    })
-}, {
-    // threshold: 1,
-    rootMargin:"-100px",
-})
-b3.forEach(b3 =>{
-    observer.observe(b3)
-})
-const first = document.querySelectorAll(".first")
-
-const observer3 = new IntersectionObserver(entries => {
-    
-    entries.forEach(entry =>{
-        entry.target.classList.toggle("show", entry.isIntersecting)
-
-        // the berlow commented code is for if you don't want the effect to be repetitive
-        // if (entry.isIntersecting) observer.unobserve(entry.target)
-        console.log(entries)
-    })
-}, {
-    // threshold: 1,
-    rootMargin:"-100px",
-})
-first.forEach(first =>{
-    observer.observe(first)
-})
-const maintxt = document.querySelectorAll(".maintxt")
-
-const observer4 = new IntersectionObserver(entries => {
-    
-    entries.forEach(entry =>{
-        entry.target.classList.toggle("show", entry.isIntersecting)
-
-        // the berlow commented code is for if you don't want the effect to be repetitive
-        // if (entry.isIntersecting) observer.unobserve(entry.target)
-        console.log(entries)
-    })
-}, {
-    // threshold: 1,
-    rootMargin:"-100px",
-})
-maintxt.forEach(maintxt =>{
-    observer.observe(maintxt)
-})
-
-const txt2 = document.querySelectorAll(".txt2")
-
-const observer5 = new IntersectionObserver(entries => {
-    
-    entries.forEach(entry =>{
-        entry.target.classList.toggle("show", entry.isIntersecting)
-
-        // the berlow commented code is for if you don't want the effect to be repetitive
-        // if (entry.isIntersecting) observer.unobserve(entry.target)
-        console.log(entries)
-    })
-}, {
-    // threshold: 1,
-    rootMargin:"-100px",
-})
-txt2.forEach(txt2 =>{
-    observer.observe(txt2)
-})
-
-const txt3 = document.querySelectorAll(".txt3")
-
-const observer6 = new IntersectionObserver(entries => {
-    
-    entries.forEach(entry =>{
-        entry.target.classList.toggle("show", entry.isIntersecting)
-
-        // the berlow commented code is for if you don't want the effect to be repetitive
-        // if (entry.isIntersecting) observer.unobserve(entry.target)
-        console.log(entries)
-    })
-}, {
-    // threshold: 1,
-    rootMargin:"-100px",
-})
-txt3.forEach(txt3 =>{
-    observer.observe(txt3)
-})
-
-const textbox2 = document.querySelectorAll(".textbox2")
-
-const observer9 = new IntersectionObserver(entries => {
-    
-    entries.forEach(entry =>{
-        entry.target.classList.toggle("show", entry.isIntersecting)
-
-        // the berlow commented code is for if you don't want the effect to be repetitive
-        // if (entry.isIntersecting) observer.unobserve(entry.target)
-        console.log(entries)
-    })
-}, {
-    // threshold: 1,
-    rootMargin:"-100px",
-})
-textbox2.forEach(textbox2 =>{
-    observer.observe(textbox2)
-})
-
-const textbox3 = document.querySelectorAll(".textbox3")
-
-const observer10 = new IntersectionObserver(entries => {
-    
-    entries.forEach(entry =>{
-        entry.target.classList.toggle("show", entry.isIntersecting)
-
-        // the berlow commented code is for if you don't want the effect to be repetitive
-        // if (entry.isIntersecting) observer.unobserve(entry.target)
-        console.log(entries)
-    })
-}, {
-    // threshold: 1,
-    rootMargin:"-100px",
-})
-textbox3.forEach(textbox3 =>{
-    observer.observe(textbox3)
-})
-
-const textbox4 = document.querySelectorAll(".textbox4")
-
-const observer11 = new IntersectionObserver(entries => {
-    
-    entries.forEach(entry =>{
-        entry.target.classList.toggle("show", entry.isIntersecting)
-
-        // the berlow commented code is for if you don't want the effect to be repetitive
-        // if (entry.isIntersecting) observer.unobserve(entry.target)
-        console.log(entries)
-    })
-}, {
-    // threshold: 1,
-    rootMargin:"-100px",
-})
-textbox4.forEach(textbox4 =>{
-    observer.observe(textbox4)
-})
-
-const textbox5 = document.querySelectorAll(".textbox5")
-
-const observer12 = new IntersectionObserver(entries => {
-    
-    entries.forEach(entry =>{
-        entry.target.classList.toggle("show", entry.isIntersecting)
-
-        // the berlow commented code is for if you don't want the effect to be repetitive
-        // if (entry.isIntersecting) observer.unobserve(entry.target)
-        console.log(entries)
-    })
-}, {
-    // threshold: 1,
-    rootMargin:"100px",
-})
-textbox5.forEach(textbox5=>{
-    observer.observe(textbox5)
+firsttext.forEach(firsttext=>{
+    observer.observe(firsttext)
 })
 
